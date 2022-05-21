e7335ff344dc0d225c741ce8bc2a4b0be93d2a59
Clacarli <103129545+Clacarli@users.noreply.github.com>
2022-05-21 07:38:36 +0200

    API weather cities

--- src/WMAapp.js
+++ src/WMAapp.js
@@ -1,1 +1,17 @@
 //alert("Ciao you!");
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureItem = document.querySelector("#temperature");
+  let cityItem = document.querySelector("#cities");
+  let conditionsItem = document.querySelector("#conditions");
+  let windItem = document.querySelector("#winds");
+  temperatureItem.innerHTML = Math.round(response.data.main.temp);
+  cityItem.innerHTML = response.data.name;
+  conditionsItem.innerHTML = response.data.weather[0].description;
+  windItem.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "8c1d3a291a4d37607365dab69374db49";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Firenze&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
